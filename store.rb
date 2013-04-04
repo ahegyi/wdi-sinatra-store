ef98eafece37bf33e03fd465da4f2dc5359a1059
Aron Hegyi <ahegyi@gmail.com>
2013-04-04 16:58:16 -0700

    Full CRUD functionality for Products plus a cool background header (way too many updates...)

--- store.rb
+++ store.rb
@@ -5,6 +5,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 # require 'sinatra/support/numeric'
 require 'sqlite3'
+gem 'binding_of_caller'
+require 'binding_of_caller'
+require 'better_errors'
 
 =begin
 class Main < Sinatra::Base
@@ -21,64 +24,129 @@ end
 set :run, true
 set :port, 6787
 
-get '/users' do
-  redirect '/users/sort/name/asc'
+before do
+  @db = SQLite3::Database.new "store.sqlite3"
+  @db.results_as_hash = true
 end
 
-get '/users/sort/:orderby/:dir' do
-  db = SQLite3::Database.new "store.sqlite3"
-  db.results_as_hash = true
-  @rs = db.prepare("SELECT * FROM users ORDER BY #{params[:orderby]} #{params[:dir].upcase};").execute
-  @sort = params
-
-  erb :show_users
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path("..", __FILE__)
 end
 
+# ================== CURRENCY HELPER ================== #
+
+class Numeric
+  def to_currency(currency="$")
+    formatted_float = sprintf("%.2f", self)
+    return currency + formatted_float.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, '\1,')
+  end
+end
+
+# ================== HOME ================== #
+
+get '/' do
+  erb :home
+end
+
+# ================== PRODUCTS ================== #
+
+# ================== GET PRODUCTS ================== #
+
+# GET ALL PRODUCTS
 get '/products' do
   redirect '/products/sort/name/asc'
 end
 
-get '/products/sort/:orderby/:dir' do
-  db = SQLite3::Database.new "store.sqlite3"
-  db.results_as_hash = true
-  @rs = db.execute("SELECT * FROM products ORDER BY #{params[:orderby]} #{params[:dir].upcase};")
-#  @rsquestion = db.execute("SELECT * FROM products ORDER BY ? ?;", params[:orderby], params[:dir].upcase)
+# SHOW FORM FOR CREATING A NEW PRODUCT
+get '/products/new' do
+  @product_details = Hash.new("") # Fake-out for new_product.erb, which uses product_form.erb
 
+  @form_details = {}
+  @form_details[:post_path] = '/products'
+  @form_details[:legend] = "New Product"
+
+  erb :new_product
+end
+
+# GET PRODUCT DETAIL
+get '/products/:id' do
+  @id = params[:id]
+
+  @rs = @db.execute("SELECT * FROM products WHERE id = ? LIMIT 1", @id)
+  @details = @rs.first
+
+  erb :show_product_detail
+end
+
+# SHOW FORM FOR EDITING A PRODUCT
+get '/products/:id/edit' do
+  @id = params[:id]
+
+  @rs = @db.execute("SELECT * FROM products WHERE id = ? LIMIT 1", @id)
+  @product_details = @rs.first
+  
+  @form_details = {}
+  @form_details[:post_path] = "/products/#{@product_details['id']}"
+  @form_details[:legend] = "Update Product"
+
+  erb :update_product
+end
+
+# GET ALL PRODUCTS (with sorting)
+get '/products/sort/:orderby/:dir' do
+  @rs = @db.execute("SELECT * FROM products ORDER BY #{params[:orderby]} #{params[:dir].upcase};")
   @sort = params
 
   erb :show_products
 end
 
-get '/query' do
-  "|" + request.query_string + "|" + params.inspect
+# ================== POST/PUT/DELETE PRODUCTS ================== #
+
+
+# CREATE PRODUCT
+post '/products' do
+  @name = params[:product_name]
+  @price = params[:product_price].to_f
+  @created_at = Time.now
+
+  @rs = @db.execute("INSERT INTO products ('created_at', 'name', 'price') VALUES (?, ?, ?);", @created_at.to_s, @name, @price)
+
+  erb :product_created
 end
 
-get '/' do
-  erb :home
+# UPDATE PRODUCT
+post '/products/:id' do
+  @id = params[:id]
+  @name = params[:product_name]
+  @price = params[:product_price].to_f
+
+  @rs = @db.execute("UPDATE products SET name = ?, price = ? WHERE id = ?;", @name, @price, @id)
+  
+  redirect '/products'
 end
- 
- 
- 
- 
- 
- 
- 
- 
- 
-=begin
- 
-  <form method='post' action='/create'>
-    <input type='text' name='name' autofocus>
-    <input type='text' name='photo'>
-    <input type='text' name='breed'>
-    <button>dog me!</button>
-  </form>
- 
- 
-  post '/create' do
-  end
- 
- 
-  redirect '/'
- 
-=end
+
+# DELETE PRODUCT
+post '/products/:id/destroy' do
+  @id = params[:id]
+  @rs = @db.execute("DELETE FROM products WHERE id = ?", @id)
+
+  redirect '/products'
+end
+
+# ================== USERS ================== #
+
+
+
+# ================== GET USERS ================== #
+
+get '/users' do
+  redirect '/users/sort/name/asc'
+end
+
+get '/users/sort/:orderby/:dir' do
+  @rs = @db.prepare("SELECT * FROM users ORDER BY #{params[:orderby]} #{params[:dir].upcase};").execute
+  @sort = params
+
+  erb :show_users
+end
